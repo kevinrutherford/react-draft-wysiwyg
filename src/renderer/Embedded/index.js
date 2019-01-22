0c4dbe2edb93bafaad80faf196ee59c526b3a264
Kevin Rutherford <kevin@rutherford-software.com>
2019-01-22 14:52:33 +0000

    Add Bootstrap responsive styles around embedded content

--- src/renderer/Embedded/index.js
+++ src/renderer/Embedded/index.js
@@ -4,7 +4,19 @@ import PropTypes from 'prop-types';
 const Embed = ({ block, contentState }) => {
   const entity = contentState.getEntity(block.getEntityAt(0));
   const { src, height, width } = entity.getData();
-  return (<iframe height={height} width={width} src={src} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" />);
+  return (
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe
+        className='embed-responsive-item'
+        height={height}
+        width={width}
+        src={src}
+        frameBorder="0"
+        allowFullScreen
+        title="Embedded content"
+        />
+    </div>
+  )
 };
 
 Embed.propTypes = {
@@ -13,3 +25,4 @@ Embed.propTypes = {
 };
 
 export default Embed;
+
