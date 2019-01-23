a163f6a3b52d0a030578929a51287504e787c0a6
Kevin Rutherford <kevin@rutherford-software.com>
2019-01-23 13:10:47 +0000

    Fix stupid typo

--- src/renderer/Embedded/index.js
+++ src/renderer/Embedded/index.js
@@ -5,7 +5,7 @@ const Embed = ({ block, contentState }) => {
   const entity = contentState.getEntity(block.getEntityAt(0));
   const { src, height, width } = entity.getData();
   return (
-    <div class="embed-responsive embed-responsive-16by9">
+    <div className="embed-responsive embed-responsive-16by9">
       <iframe
         className='embed-responsive-item'
         height={height}
